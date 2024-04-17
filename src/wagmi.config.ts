6a913dd810c001bcd6a76984a628f8ef6c861a50
zhaxzhax <hizhaxzhax@gmail.com>
2024-04-17 13:32:51 -0700

    integrate walletConnect

--- src/wagmi.config.ts
+++ src/wagmi.config.ts
@@ -2,16 +2,19 @@ import { http, createConfig } from 'wagmi'
 import { base, mainnet, optimism,sepolia } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
-
 export const config = createConfig({
-  chains: [mainnet, base,sepolia],
+  chains: [sepolia],
   connectors: [
+    // safe({
+    //     allowedDomains: [/app.safe.global$/],
+    //     debug: false,
+    // }),
     injected(),
-    safe(),
+    walletConnect({projectId:'4f1f810cbef4e6b668d7f74d86c77cc8'})
   ],
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
+    // [mainnet.id]: http(),
+    // [base.id]: http(),
     [sepolia.id]: http(),
   },
 })
